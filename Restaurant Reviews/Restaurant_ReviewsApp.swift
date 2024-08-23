e3a13fdf92155f68d73040b48d04bcb1516d78b6
jdelano <jdelano@cedarville.edu>
2024-08-23 14:21:34 -0400

    Initial Commit

---
+++ Restaurant Reviews/Restaurant_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ReviewsApp.swift
+//  Restaurant Reviews
+//
+//  Created by John Delano on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
